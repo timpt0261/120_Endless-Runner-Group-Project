a730130fdd10105431f2d7d769da4d0cb8e34726
timpt0261 <rchavez0215@gmail.com>
2022-04-13 23:21:07 -0700

    added files and website

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 780,
+}
+
+let game = new Phaser.Game(config);
+
+// set UI sizes
+let borderUISize = game.config.height / 15;
+let borderPadding = borderUISize / 3;
+
+// reserve keyboard vars
+let keyF, keyR, keyLEFT, keyRIGHT;
