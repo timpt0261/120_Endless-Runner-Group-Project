9c017d93a884cfc3dfb8f60f966a136d262bbac3
timpt0261 <rchavez0215@gmail.com>
2022-04-18 19:27:16 -0700

    renamed files

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -8,6 +8,12 @@ class Play extends Phaser.Scene{
     }
     // initialize gameObjects , and add assets as textures
     create(){
+        
+        // define keys
+        keyUP = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP);
+        keyDOWN= this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
 
     }
     // update things in scene
