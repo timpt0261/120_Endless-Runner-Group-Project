b502b07d622cac9eefaa7d0bed5a310001ac49e0
timpt0261 <rchavez0215@gmail.com>
2022-04-19 20:05:47 -0700

    Added to ball

--- src/prefabs/ball.js
+++ src/prefabs/ball.js
@@ -1,26 +1,13 @@
-// ball prefabs
+//Ball prefab
 class Ball extends Phaser.GameObjects.Sprite{
-    constructor(scene, x, y, texture,frame){
-        super(scene,x,y,texture, frame);
+    constructor(scene, x, y, texture, frame){
+        super(scene,x,y,texture,frame);
 
-        // add object to existing scene
+        // add  object to sceneS
         scene.add.existing(this);
-        this.moving = true;
-        this.moveSpeed = 5;
     }
 
     update(){
-
-        // left/right movement
-        if(keyLEFT.isDown && this.x  >= borderUISize + this.width){
-            this.x -= this.moveSpeed;
-        } else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
-            this.x += this.moveSpeed;
-        }
-    }
-    // reset rocket to "ground"
-    reset(){
-        this.isFiring = false;
-        this.y = game.config.height - borderUISize - borderPadding;
+        
     }
 }
