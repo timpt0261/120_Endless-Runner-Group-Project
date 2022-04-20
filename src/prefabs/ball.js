079f6786c271c37e4da8a9e54b6b43243ab339e1
timpt0261 <rchavez0215@gmail.com>
2022-04-19 19:34:07 -0700

    Postioned Ball

--- src/prefabs/ball.js
+++ src/prefabs/ball.js
@@ -5,22 +5,15 @@ class Ball extends Phaser.GameObjects.Sprite{
 
         // add object to existing scene
         scene.add.existing(this);
-        this.moving = true;
-        this.moveSpeed = 5;
     }
 
     update(){
 
-        // left/right movement
-        if(keyLEFT.isDown && this.x  >= borderUISize + this.width){
-            this.x -= this.moveSpeed;
-        } else if(keyRIGHT.isDown && this.x <= game.config.width - borderUISize - this.width){
-            this.x += this.moveSpeed;
-        }
+        
     }
     // reset rocket to "ground"
     reset(){
-        this.isFiring = false;
+        
         this.y = game.config.height - borderUISize - borderPadding;
     }
 }
