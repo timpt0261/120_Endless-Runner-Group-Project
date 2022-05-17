1891f97793ba4e26ff7feafe34a30143abbcf04b
timpt0261 <rchavez0215@gmail.com>
2022-05-16 21:57:54 -0700

    Added creative tilt

--- src/Main.js
+++ src/Main.js
@@ -23,3 +23,9 @@ let borderPadding = borderUISize / 3;
 // reserve keyboard vars
 let keySPACE, keyR, keyLEFT, keyRIGHT;
 
+// Creative Tilt
+
+// Something interesting about our game is that when the ball hits the paddle a function determines weather
+// the ball is closer to the left side, right side, or directly in the middle of the skateboard.
+// As well, that the ball's velocity is changed by the difference. An artistic choice we is that when the function runs it plays
+// a small animation of the skateboard rolling over whenever it hits, to suggest the ball and skateboard has collided.
